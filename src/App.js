4cf5db37839cfc7a67e413a51abc472aec5de98f
Kathoey <nicholasnguyen75@gmail.com>
2023-12-07 12:11:34 -0500

    completed app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  async function fetchPosts() {
+    let col = []
+    let response = await fetch('https://fakestoreapi.com/products?limit=5&sort=desc')
+    .then((response) => response.json())
+    Object.values(response).forEach(
+      x => col.push(x)
+    )
+    setPosts(col)
+  } 
+
+  useEffect(() => {
+    fetchPosts()
+    console.log(posts)
+  }, [])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+        {posts.map((item) => (
+        <div>
+        <br></br>
+        <li>
+          <div>ID: {item.id}</div>
+          <div>Title: {item.title}</div>
+          
+          <div>Image: 
+            <img src={item.image}></img>
+          </div>
+          <div>Description: {item.description}</div>
+          <div>Price: {item.price}</div>
+        </li>
+        </div>
+        ))}
+        </ul>
+        
       </header>
     </div>
   );
